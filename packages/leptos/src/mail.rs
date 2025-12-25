f4da5a4311bb5e480db7de33a32692c858b2caec
KwachOjunga <ojungakwach@gmail.com>
2025-12-25 06:22:50 +0300

    -- consolidate mail icons

--- packages/leptos/src/mail.rs
+++ packages/leptos/src/mail.rs
@@ -34,3 +34,441 @@ pub fn Mail(
         </svg>
     }
 }
+#[component]
+pub fn Mail(
+    #[prop(default = 24.into(), into)] size: Signal<usize>,
+    #[prop(default = "currentColor".into(), into)] color: Signal<String>,
+    #[prop(default = "none".into(), into)] fill: Signal<String>,
+    #[prop(default = 2.into(), into)] stroke_width: Signal<usize>,
+    #[prop(default = false.into(), into)] absolute_stroke_width: Signal<bool>,
+    #[prop(optional)] node_ref: NodeRef<Svg>,
+) -> impl IntoView {
+    let stroke_width = Signal::derive(move || {
+        if absolute_stroke_width.get() {
+            stroke_width.get() * 24 / size.get()
+        } else {
+            stroke_width.get()
+        }
+    });
+    view! {
+        <svg
+            node_ref=node_ref
+            class:lucide=true
+            xmlns="http://www.w3.org/2000/svg"
+            width=size
+            height=size
+            viewBox="0 0 24 24"
+            fill=fill
+            stroke=color
+            stroke-width=stroke_width
+            stroke-linecap="round"
+            stroke-linejoin="round"
+        >
+            <path d="m22 7-8.991 5.727a2 2 0 0 1-2.009 0L2 7" />
+            <rect x="2" y="4" width="20" height="16" rx="2" />
+        </svg>
+    }
+}
+#[component]
+pub fn MailCheck(
+    #[prop(default = 24.into(), into)] size: Signal<usize>,
+    #[prop(default = "currentColor".into(), into)] color: Signal<String>,
+    #[prop(default = "none".into(), into)] fill: Signal<String>,
+    #[prop(default = 2.into(), into)] stroke_width: Signal<usize>,
+    #[prop(default = false.into(), into)] absolute_stroke_width: Signal<bool>,
+    #[prop(optional)] node_ref: NodeRef<Svg>,
+) -> impl IntoView {
+    let stroke_width = Signal::derive(move || {
+        if absolute_stroke_width.get() {
+            stroke_width.get() * 24 / size.get()
+        } else {
+            stroke_width.get()
+        }
+    });
+    view! {
+        <svg
+            node_ref=node_ref
+            class:lucide=true
+            xmlns="http://www.w3.org/2000/svg"
+            width=size
+            height=size
+            viewBox="0 0 24 24"
+            fill=fill
+            stroke=color
+            stroke-width=stroke_width
+            stroke-linecap="round"
+            stroke-linejoin="round"
+        >
+            <path d="M22 13V6a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2v12c0 1.1.9 2 2 2h8" />
+            <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7" />
+            <path d="m16 19 2 2 4-4" />
+        </svg>
+    }
+}
+#[component]
+pub fn MailMinus(
+    #[prop(default = 24.into(), into)] size: Signal<usize>,
+    #[prop(default = "currentColor".into(), into)] color: Signal<String>,
+    #[prop(default = "none".into(), into)] fill: Signal<String>,
+    #[prop(default = 2.into(), into)] stroke_width: Signal<usize>,
+    #[prop(default = false.into(), into)] absolute_stroke_width: Signal<bool>,
+    #[prop(optional)] node_ref: NodeRef<Svg>,
+) -> impl IntoView {
+    let stroke_width = Signal::derive(move || {
+        if absolute_stroke_width.get() {
+            stroke_width.get() * 24 / size.get()
+        } else {
+            stroke_width.get()
+        }
+    });
+    view! {
+        <svg
+            node_ref=node_ref
+            class:lucide=true
+            xmlns="http://www.w3.org/2000/svg"
+            width=size
+            height=size
+            viewBox="0 0 24 24"
+            fill=fill
+            stroke=color
+            stroke-width=stroke_width
+            stroke-linecap="round"
+            stroke-linejoin="round"
+        >
+            <path d="M22 15V6a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2v12c0 1.1.9 2 2 2h8" />
+            <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7" />
+            <path d="M16 19h6" />
+        </svg>
+    }
+}
+#[component]
+pub fn MailOpen(
+    #[prop(default = 24.into(), into)] size: Signal<usize>,
+    #[prop(default = "currentColor".into(), into)] color: Signal<String>,
+    #[prop(default = "none".into(), into)] fill: Signal<String>,
+    #[prop(default = 2.into(), into)] stroke_width: Signal<usize>,
+    #[prop(default = false.into(), into)] absolute_stroke_width: Signal<bool>,
+    #[prop(optional)] node_ref: NodeRef<Svg>,
+) -> impl IntoView {
+    let stroke_width = Signal::derive(move || {
+        if absolute_stroke_width.get() {
+            stroke_width.get() * 24 / size.get()
+        } else {
+            stroke_width.get()
+        }
+    });
+    view! {
+        <svg
+            node_ref=node_ref
+            class:lucide=true
+            xmlns="http://www.w3.org/2000/svg"
+            width=size
+            height=size
+            viewBox="0 0 24 24"
+            fill=fill
+            stroke=color
+            stroke-width=stroke_width
+            stroke-linecap="round"
+            stroke-linejoin="round"
+        >
+            <path d="M21.2 8.4c.5.38.8.97.8 1.6v10a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V10a2 2 0 0 1 .8-1.6l8-6a2 2 0 0 1 2.4 0l8 6Z" />
+            <path d="m22 10-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 10" />
+        </svg>
+    }
+}
+#[component]
+pub fn MailPlus(
+    #[prop(default = 24.into(), into)] size: Signal<usize>,
+    #[prop(default = "currentColor".into(), into)] color: Signal<String>,
+    #[prop(default = "none".into(), into)] fill: Signal<String>,
+    #[prop(default = 2.into(), into)] stroke_width: Signal<usize>,
+    #[prop(default = false.into(), into)] absolute_stroke_width: Signal<bool>,
+    #[prop(optional)] node_ref: NodeRef<Svg>,
+) -> impl IntoView {
+    let stroke_width = Signal::derive(move || {
+        if absolute_stroke_width.get() {
+            stroke_width.get() * 24 / size.get()
+        } else {
+            stroke_width.get()
+        }
+    });
+    view! {
+        <svg
+            node_ref=node_ref
+            class:lucide=true
+            xmlns="http://www.w3.org/2000/svg"
+            width=size
+            height=size
+            viewBox="0 0 24 24"
+            fill=fill
+            stroke=color
+            stroke-width=stroke_width
+            stroke-linecap="round"
+            stroke-linejoin="round"
+        >
+            <path d="M22 13V6a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2v12c0 1.1.9 2 2 2h8" />
+            <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7" />
+            <path d="M19 16v6" />
+            <path d="M16 19h6" />
+        </svg>
+    }
+}
+#[component]
+pub fn MailQuestionMark(
+    #[prop(default = 24.into(), into)] size: Signal<usize>,
+    #[prop(default = "currentColor".into(), into)] color: Signal<String>,
+    #[prop(default = "none".into(), into)] fill: Signal<String>,
+    #[prop(default = 2.into(), into)] stroke_width: Signal<usize>,
+    #[prop(default = false.into(), into)] absolute_stroke_width: Signal<bool>,
+    #[prop(optional)] node_ref: NodeRef<Svg>,
+) -> impl IntoView {
+    let stroke_width = Signal::derive(move || {
+        if absolute_stroke_width.get() {
+            stroke_width.get() * 24 / size.get()
+        } else {
+            stroke_width.get()
+        }
+    });
+    view! {
+        <svg
+            node_ref=node_ref
+            class:lucide=true
+            xmlns="http://www.w3.org/2000/svg"
+            width=size
+            height=size
+            viewBox="0 0 24 24"
+            fill=fill
+            stroke=color
+            stroke-width=stroke_width
+            stroke-linecap="round"
+            stroke-linejoin="round"
+        >
+            <path d="M22 10.5V6a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2v12c0 1.1.9 2 2 2h12.5" />
+            <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7" />
+            <path d="M18 15.28c.2-.4.5-.8.9-1a2.1 2.1 0 0 1 2.6.4c.3.4.5.8.5 1.3 0 1.3-2 2-2 2" />
+            <path d="M20 22v.01" />
+        </svg>
+    }
+}
+#[component]
+pub fn MailQuestion(
+    #[prop(default = 24.into(), into)] size: Signal<usize>,
+    #[prop(default = "currentColor".into(), into)] color: Signal<String>,
+    #[prop(default = "none".into(), into)] fill: Signal<String>,
+    #[prop(default = 2.into(), into)] stroke_width: Signal<usize>,
+    #[prop(default = false.into(), into)] absolute_stroke_width: Signal<bool>,
+    #[prop(optional)] node_ref: NodeRef<Svg>,
+) -> impl IntoView {
+    let stroke_width = Signal::derive(move || {
+        if absolute_stroke_width.get() {
+            stroke_width.get() * 24 / size.get()
+        } else {
+            stroke_width.get()
+        }
+    });
+    view! {
+        <svg
+            node_ref=node_ref
+            class:lucide=true
+            xmlns="http://www.w3.org/2000/svg"
+            width=size
+            height=size
+            viewBox="0 0 24 24"
+            fill=fill
+            stroke=color
+            stroke-width=stroke_width
+            stroke-linecap="round"
+            stroke-linejoin="round"
+        >
+            <path d="M22 10.5V6a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2v12c0 1.1.9 2 2 2h12.5" />
+            <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7" />
+            <path d="M18 15.28c.2-.4.5-.8.9-1a2.1 2.1 0 0 1 2.6.4c.3.4.5.8.5 1.3 0 1.3-2 2-2 2" />
+            <path d="M20 22v.01" />
+        </svg>
+    }
+}
+#[component]
+pub fn MailSearch(
+    #[prop(default = 24.into(), into)] size: Signal<usize>,
+    #[prop(default = "currentColor".into(), into)] color: Signal<String>,
+    #[prop(default = "none".into(), into)] fill: Signal<String>,
+    #[prop(default = 2.into(), into)] stroke_width: Signal<usize>,
+    #[prop(default = false.into(), into)] absolute_stroke_width: Signal<bool>,
+    #[prop(optional)] node_ref: NodeRef<Svg>,
+) -> impl IntoView {
+    let stroke_width = Signal::derive(move || {
+        if absolute_stroke_width.get() {
+            stroke_width.get() * 24 / size.get()
+        } else {
+            stroke_width.get()
+        }
+    });
+    view! {
+        <svg
+            node_ref=node_ref
+            class:lucide=true
+            xmlns="http://www.w3.org/2000/svg"
+            width=size
+            height=size
+            viewBox="0 0 24 24"
+            fill=fill
+            stroke=color
+            stroke-width=stroke_width
+            stroke-linecap="round"
+            stroke-linejoin="round"
+        >
+            <path d="M22 12.5V6a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2v12c0 1.1.9 2 2 2h7.5" />
+            <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7" />
+            <path d="M18 21a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" />
+            <circle cx="18" cy="18" r="3" />
+            <path d="m22 22-1.5-1.5" />
+        </svg>
+    }
+}
+#[component]
+pub fn MailWarning(
+    #[prop(default = 24.into(), into)] size: Signal<usize>,
+    #[prop(default = "currentColor".into(), into)] color: Signal<String>,
+    #[prop(default = "none".into(), into)] fill: Signal<String>,
+    #[prop(default = 2.into(), into)] stroke_width: Signal<usize>,
+    #[prop(default = false.into(), into)] absolute_stroke_width: Signal<bool>,
+    #[prop(optional)] node_ref: NodeRef<Svg>,
+) -> impl IntoView {
+    let stroke_width = Signal::derive(move || {
+        if absolute_stroke_width.get() {
+            stroke_width.get() * 24 / size.get()
+        } else {
+            stroke_width.get()
+        }
+    });
+    view! {
+        <svg
+            node_ref=node_ref
+            class:lucide=true
+            xmlns="http://www.w3.org/2000/svg"
+            width=size
+            height=size
+            viewBox="0 0 24 24"
+            fill=fill
+            stroke=color
+            stroke-width=stroke_width
+            stroke-linecap="round"
+            stroke-linejoin="round"
+        >
+            <path d="M22 10.5V6a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2v12c0 1.1.9 2 2 2h12.5" />
+            <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7" />
+            <path d="M20 14v4" />
+            <path d="M20 22v.01" />
+        </svg>
+    }
+}
+#[component]
+pub fn MailX(
+    #[prop(default = 24.into(), into)] size: Signal<usize>,
+    #[prop(default = "currentColor".into(), into)] color: Signal<String>,
+    #[prop(default = "none".into(), into)] fill: Signal<String>,
+    #[prop(default = 2.into(), into)] stroke_width: Signal<usize>,
+    #[prop(default = false.into(), into)] absolute_stroke_width: Signal<bool>,
+    #[prop(optional)] node_ref: NodeRef<Svg>,
+) -> impl IntoView {
+    let stroke_width = Signal::derive(move || {
+        if absolute_stroke_width.get() {
+            stroke_width.get() * 24 / size.get()
+        } else {
+            stroke_width.get()
+        }
+    });
+    view! {
+        <svg
+            node_ref=node_ref
+            class:lucide=true
+            xmlns="http://www.w3.org/2000/svg"
+            width=size
+            height=size
+            viewBox="0 0 24 24"
+            fill=fill
+            stroke=color
+            stroke-width=stroke_width
+            stroke-linecap="round"
+            stroke-linejoin="round"
+        >
+            <path d="M22 13V6a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2v12c0 1.1.9 2 2 2h9" />
+            <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7" />
+            <path d="m17 17 4 4" />
+            <path d="m21 17-4 4" />
+        </svg>
+    }
+}
+#[component]
+pub fn Mails(
+    #[prop(default = 24.into(), into)] size: Signal<usize>,
+    #[prop(default = "currentColor".into(), into)] color: Signal<String>,
+    #[prop(default = "none".into(), into)] fill: Signal<String>,
+    #[prop(default = 2.into(), into)] stroke_width: Signal<usize>,
+    #[prop(default = false.into(), into)] absolute_stroke_width: Signal<bool>,
+    #[prop(optional)] node_ref: NodeRef<Svg>,
+) -> impl IntoView {
+    let stroke_width = Signal::derive(move || {
+        if absolute_stroke_width.get() {
+            stroke_width.get() * 24 / size.get()
+        } else {
+            stroke_width.get()
+        }
+    });
+    view! {
+        <svg
+            node_ref=node_ref
+            class:lucide=true
+            xmlns="http://www.w3.org/2000/svg"
+            width=size
+            height=size
+            viewBox="0 0 24 24"
+            fill=fill
+            stroke=color
+            stroke-width=stroke_width
+            stroke-linecap="round"
+            stroke-linejoin="round"
+        >
+            <path d="M17 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2v-8a2 2 0 0 1 1-1.732" />
+            <path d="m22 5.5-6.419 4.179a2 2 0 0 1-2.162 0L7 5.5" />
+            <rect x="7" y="3" width="15" height="12" rx="2" />
+        </svg>
+    }
+}
+#[component]
+pub fn Mailbox(
+    #[prop(default = 24.into(), into)] size: Signal<usize>,
+    #[prop(default = "currentColor".into(), into)] color: Signal<String>,
+    #[prop(default = "none".into(), into)] fill: Signal<String>,
+    #[prop(default = 2.into(), into)] stroke_width: Signal<usize>,
+    #[prop(default = false.into(), into)] absolute_stroke_width: Signal<bool>,
+    #[prop(optional)] node_ref: NodeRef<Svg>,
+) -> impl IntoView {
+    let stroke_width = Signal::derive(move || {
+        if absolute_stroke_width.get() {
+            stroke_width.get() * 24 / size.get()
+        } else {
+            stroke_width.get()
+        }
+    });
+    view! {
+        <svg
+            node_ref=node_ref
+            class:lucide=true
+            xmlns="http://www.w3.org/2000/svg"
+            width=size
+            height=size
+            viewBox="0 0 24 24"
+            fill=fill
+            stroke=color
+            stroke-width=stroke_width
+            stroke-linecap="round"
+            stroke-linejoin="round"
+        >
+            <path d="M22 17a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V9.5C2 7 4 5 6.5 5H18c2.2 0 4 1.8 4 4v8Z" />
+            <polyline points="15,9 18,9 18,11" />
+            <path d="M6.5 5C9 5 11 7 11 9.5V17a2 2 0 0 1-2 2" />
+            <line x1="6" x2="7" y1="10" y2="10" />
+        </svg>
+    }
+}
